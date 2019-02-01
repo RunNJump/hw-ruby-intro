c2c64c120fc0ca08d683644c119ca0e36bdd05a4
Hong Seok Jang <hongseok.jang@berkeley.edu>
2019-01-31 19:54:45 -0800

    finish

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,65 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  total = 0
+  arr.each {|x| total += x}
+  return total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+  	return 0
+  elsif arr.length == 1
+  	return arr[0]
+  else
+  	arr.sort!
+  	return arr[-1] + arr[-2]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  hash = Hash.new
+  arr.each do |x|
+  	if hash.key?(x)
+  		return true
+  	end
+  	hash[n-x] = x
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + "#{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return s[0] =~ /^[b-df-hj-np-tv-z]/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /^[0-1]/
+  	return s.to_i(2) % 4 == 0
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+	attr_accessor :isbn, :price
+
+	def initialize(isbn, price)
+		if isbn.empty?() || price <= 0
+			raise ArgumentError.new()
+		end
+		@isbn = isbn
+		@price = price
+	end
+
+	def price_as_string
+		return "$" + @price
+	end
+
 end
